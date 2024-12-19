e01d89aa6cefe9b7a9d083818503abaad1658275
Carlos <carlosjorgech7@gmail.com>
2024-12-19 20:11:29 +0100

    update api

--- src/app/home/services/places-watter.service.ts
+++ src/app/home/services/places-watter.service.ts
@@ -11,7 +11,7 @@ export class PlacesWatterService {
 
   public getFountaingWatter(): Observable<FountainData[]> {
     return this.http.get<FountainData[]>(
-      'https://planfit-be.onrender.com/fuentes_de_beber/'
+      'https://planfit-be-1.onrender.com/fuentes_de_beber/'
     );
   }
 }
